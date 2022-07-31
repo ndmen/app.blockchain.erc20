5757d3a61697fd117f44cd6ded25905198fae4d5
Artem Abdullin <artemleelooai@gmail.com>
2022-07-31 23:57:34 +0300

    Added ERC20 token contract

---
+++ ERC20.sol
@@ -0,0 +1,19 @@
+// Example of ERC20 token contract.
+
+// SPDX-License-Idenifier: MIT
+pragma solidity ^0.8.13;
+
+import "./IERC20.sol";
+
+contract ERC20 is IERC20 {
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+    string public name = "Solidity by Example";
+    string public symbol = "SOLBYEX";
+    uint8 public decimals = 18;
+
+    function transfer(address recipient, uint amount) external returns (bool) {
+        
+    }
+}
