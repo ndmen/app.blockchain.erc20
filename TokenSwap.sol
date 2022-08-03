fe863e856172641a6070c785d77ec4a6d141e412
Artem Abdullin <artemleelooai@gmail.com>
2022-08-03 23:49:59 +0300

    Added constructor to contract TokenSwap

--- TokenSwap.sol
+++ TokenSwap.sol
@@ -26,5 +26,31 @@ How to swap tokens
 */
 
 contract TokenSwap {
-    
+    IERC20 public token1;
+    address public owner1;
+    uint public amount1;
+    IERC20 public token2;
+    address public owner2;
+    uint public amount2;
+
+    constructor(
+        address _token1,
+        address _owner1,
+        uint _amount1,
+        address _token2,
+        address _owner2,
+        uint _amount2
+    ) {
+        token1 = IERC20(_token1);
+        owner1 = _owner1;
+        amount11 = _amount1;
+        token2 = IERC20(_token2);
+        owner2 = _owner2;
+        amount2 = _amount2;
+    }
+
+    function swap() public {
+        require(msg.sender == owner1 || msg.sender == owner2, "Not authorized");
+
+    }
 }
