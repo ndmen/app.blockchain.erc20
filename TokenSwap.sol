51e82ef8bf28491cf5f3b065beea8fa974141c8b
Artem Abdullin <artemleelooai@gmail.com>
2022-08-03 23:54:35 +0300

    Added function swap

--- TokenSwap.sol
+++ TokenSwap.sol
@@ -51,6 +51,16 @@ contract TokenSwap {
 
     function swap() public {
         require(msg.sender == owner1 || msg.sender == owner2, "Not authorized");
+        require(
+            token1.allowance(owner1, address(this)) >= amount1,
+            "Token 1 allowance too low"
+        );
+        require(
+            token2.allowance(owner2, address(this)) >= amount2,
+            "Token 2 allowance too low"
+        );
 
+        _safeTransferFrom(token1, owner1, owner2, amount1);
+        _safeTransferFrom(token2, owner2, owner1, amount2);
     }
 }
